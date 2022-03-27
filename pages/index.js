ffb205a02313f5f223f22caa528248cb74606811
Stepanyan Stepan <Done.Foor@yandex.ru>
2022-03-28 02:15:21 +0300

    api get request + url parameter filter

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,32 @@ const form = document.forms.form;
 const closeButton = document.querySelectorAll('.closeAddPopup');
 const popup = document.querySelector('.popup');
 const button = document.querySelector('.about__form-button');
+const preloader = document.querySelector('.preloader');
+const table = document.querySelector('.table');
+const tablerow = document.querySelector('.table__info');
 
-button.addEventListener('click', function(evt){
+button.addEventListener('click', function(){
     popup.classList.add('popup_opened');
 })
 
 closeButton.forEach(element =>{
-    element.addEventListener('click', function(element){
+    element.addEventListener('click', function(){
         popup.classList.remove('popup_opened');
     })
 })
 
 form.addEventListener('submit', function (evt) {
     evt.preventDefault();
-    fetch('https://jsonplaceholder.typicode.com/todos')
-    .then((res) => {
-        console.log(res);
+    fetch('https://jsonplaceholder.typicode.com/todos?userId=5&completed=false')
+    .then((res) => res.text())
+    .then(data => {
+        preloader.classList.add('preloader_onload');
+        console.log(data);
         popup.classList.remove('popup_opened');
+ 
+        document.querySelector('#id').innerText = data.id;
+        document.querySelector('#title').innerText = data.title;
+        table.classList.add('table_opened');  
     })
     .catch((err) => {
         console.log(err);
